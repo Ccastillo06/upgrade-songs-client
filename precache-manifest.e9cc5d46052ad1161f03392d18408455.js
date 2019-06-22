2474bdb4e16e72ce49178eaefea064d011a6211a
Ccastillo06 <ccastillon92@gmail.com>
2019-06-22 14:05:26 +0200

    Updates

---
+++ precache-manifest.e9cc5d46052ad1161f03392d18408455.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a59de3bf119458cfa430aff6c3b040c",
+    "url": "/upgrade-songs-client/index.html"
+  },
+  {
+    "revision": "a98f6e4bd1b7d7e76d93",
+    "url": "/upgrade-songs-client/static/css/main.40818001.chunk.css"
+  },
+  {
+    "revision": "259f367ed04284c18b40",
+    "url": "/upgrade-songs-client/static/js/2.fcc9e9c7.chunk.js"
+  },
+  {
+    "revision": "a98f6e4bd1b7d7e76d93",
+    "url": "/upgrade-songs-client/static/js/main.1a6bbe91.chunk.js"
+  },
+  {
+    "revision": "a2941af56b2136b4dd05",
+    "url": "/upgrade-songs-client/static/js/runtime~main.add77f86.js"
+  }
+]);
